8001938d30c21e1a822efab02bfedf2e582b898f
WasathTheekshana <wasath.vt@gmail.com>
2023-07-10 19:40:53 +0530

    feat(server): database connect and server start

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { app } from "./config/express.js";
+import { connect } from "./config/db.js";
+
+const PORT = process.env.PORT || 8080;
+const HOST = "localhost";
+
+connect()
+  .then(() => {
+    try {
+      app.listen(PORT, HOST, () => {
+        console.log(`Server is running on http://${HOST}:${PORT}`);
+      });
+    } catch (error) {
+      console.error("Error while starting the server: ", error);
+    }
+  })
+  .catch((error) => {
+    console.error("Error connecting to database: ", error);
+  });
+
