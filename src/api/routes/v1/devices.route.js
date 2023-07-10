d011c6b36d52f53fa529e27ba9bb6a388e221870
WasathTheekshana <wasath.vt@gmail.com>
2023-07-11 00:42:22 +0530

    feat(server): add device routes

--- src/api/routes/v1/devices.route.js
+++ src/api/routes/v1/devices.route.js
@@ -1,5 +1,16 @@
 import express from "express";
+import {
+  addDevice,
+  deleteDevice,
+  getDevices,
+  updateDevice,
+} from "../../controllers/device.controller.js";
 
 const router = express.Router();
+
+router.route("/").get(getDevices);
+router.route("/").delete(deleteDevice);
+router.route("/addDevice").post(addDevice);
+router.route("/").put(updateDevice);
 
 export default router;
