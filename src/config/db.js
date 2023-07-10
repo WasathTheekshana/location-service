9333483187a949a97ffb872de45805eb79ad4389
WasathTheekshana <wasath.vt@gmail.com>
2023-07-10 19:41:32 +0530

    feat(server): mongodb with mongo memory server

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+const connect = async () => {
+    const mongod = await MongoMemoryServer.create();
+    const getUri = mongod.getUri();
+
+    const db = await mongoose.connect(getUri);
+    console.log(`Database Connected Successfully ✨`);
+    return db;
+}
+
+export { connect };
