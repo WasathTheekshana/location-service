312063c6158a4dff3c4e786e56b92edb89411fce
WasathTheekshana <wasath.vt@gmail.com>
2023-07-12 01:23:07 +0530

    fix(client): logo made a link

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Logo = ({ firstWord, secondWord }) => {
   return (
-    <h1 className="text-2xl text-center mt-6">
-      <span className="font-bold"> {firstWord} </span>
-      {secondWord}
-    </h1>
+    <Link to="/">
+      <h1 className="text-2xl text-center mt-6">
+        <span className="font-bold"> {firstWord} </span>
+        {secondWord}
+      </h1>
+    </Link>
   );
 };
 
