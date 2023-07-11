ca5a11ab90345edda2de373f1998b2a9bfc4be01
WasathTheekshana <wasath.vt@gmail.com>
2023-07-11 20:16:39 +0530

    style(client): logo alignment change

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Logo = ({ firstWord, secondWord }) => {
   return (
-    <h1 className="text-2xl text-center">
+    <h1 className="text-2xl text-center mt-6">
       <span className="font-bold"> {firstWord} </span>
       {secondWord}
     </h1>
