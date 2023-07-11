c8e5fbf7e165b88c93a48c88c53c0f795b65fbe9
WasathTheekshana <wasath.vt@gmail.com>
2023-07-12 01:24:36 +0530

    fix(client): minor fix in toasts

--- client/src/components/LocationCard.jsx
+++ client/src/components/LocationCard.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import ChipButton from "./buttons/ChipButton";
+import { Link } from "react-router-dom";
 
-const LocationCard = ({ name, address, phone, onClick }) => {
+const LocationCard = ({ name, address, phone, link }) => {
   return (
-    <div className="bg-white rounded-lg shadow-md md:max-w-1/3 py-4 px-5 flex flex-row justify-between">
-      <div>
-        <h1 className="font-bold text-xl mb-3">{name}</h1>
-        <h4 className="text-gray-500 text-xs mb-2">{phone}</h4>
-        <h4 className="text-gray-500 text-xs">{address}</h4>
+    
+      <div className="bg-white rounded-lg shadow-md md:max-w-1/3 py-4 px-5 flex flex-row justify-between hover:transition transition hover:shadow-xl">
+        <div>
+          <h1 className="font-bold text-xl mb-3">{name}</h1>
+          <h4 className="text-gray-500 text-xs mb-2">{phone}</h4>
+          <h4 className="text-gray-500 text-xs">{address}</h4>
+        </div>
+        <div>
+          <Link to={link}>
+            <ChipButton />
+          </Link>
+        </div>
       </div>
-      <div>
-        <ChipButton onClick={onClick}/>
-      </div>
-    </div>
+    
   );
 };
 
--- client/src/models/AddLocation.model.jsx
+++ client/src/models/AddLocation.model.jsx
@@ -1,8 +1,9 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import NormalButton from "../components/buttons/NormalButton";
 import { useFormik } from "formik";
 import { addLocation } from "../helper/helper";
 import { Toaster, toast } from "react-hot-toast";
+import Model from "./Model";
 
 const AddLocationModel = ({ isOpen }) => {
   const [closeModel, setCloseModel] = useState(isOpen);
@@ -44,44 +45,39 @@ const AddLocationModel = ({ isOpen }) => {
 
   if (!isOpen) return null;
   return (
-    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
-      <Toaster position="top-center" reverseOrder={false} />
-      <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
-        <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col gap-10 items-center w-full bg-white outline-none focus:outline-none py-10 px-16">
-          <div>
-            <h1 className="text-2xl">Add New Location</h1>
-          </div>
-          <form
-            onSubmit={formik.handleSubmit}
-            className="w-full flex flex-col gap-5"
-          >
-            <input
-              {...formik.getFieldProps("name")}
-              type="text"
-              className="w-full border-black border rounded-lg py-2 pr-3 pl-6"
-              placeholder="Name*"
-              required
-            />
-            <input
-              {...formik.getFieldProps("address")}
-              type="text"
-              className="w-full border-black border rounded-lg py-2 pr-3 pl-6"
-              placeholder="Address"
-            />
-            <input
-              {...formik.getFieldProps("phone")}
-              type="text"
-              className="w-full border-black border rounded-lg py-2 pr-3 pl-6"
-              placeholder="Phone No."
-            />
-            <div className="flex gap-3 justify-start w-full">
-              <NormalButton text="Add Location" submit />
-              <NormalButton text="Cancel" isBorder onClick={handleClose} />
-            </div>
-          </form>
-        </div>
+    <Model>
+      <div>
+        <h1 className="text-2xl">Add New Location</h1>
       </div>
-    </div>
+      <form
+        onSubmit={formik.handleSubmit}
+        className="w-full flex flex-col gap-5"
+      >
+        <input
+          {...formik.getFieldProps("name")}
+          type="text"
+          className="w-full border-black border rounded-lg py-2 pr-3 pl-6"
+          placeholder="Name*"
+          required
+        />
+        <input
+          {...formik.getFieldProps("address")}
+          type="text"
+          className="w-full border-black border rounded-lg py-2 pr-3 pl-6"
+          placeholder="Address"
+        />
+        <input
+          {...formik.getFieldProps("phone")}
+          type="text"
+          className="w-full border-black border rounded-lg py-2 pr-3 pl-6"
+          placeholder="Phone No."
+        />
+        <div className="flex gap-3 justify-start w-full">
+          <NormalButton text="Add Location" submit />
+          <NormalButton text="Cancel" isBorder onClick={handleClose} />
+        </div>
+      </form>
+    </Model>
   );
 };
 
