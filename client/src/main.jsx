e698014b27621d3e02c334a958c2d7f7abf0144b
WasathTheekshana <wasath.vt@gmail.com>
2023-07-12 01:21:35 +0530

    fix(client): router model nested

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Landing from "./Landing.jsx";
+import LocationDetails from "./pages/LocationDetails.jsx";
 import {
-    Route,
-    createBrowserRouter,
-    createRoutesFromElements,
-    RouterProvider
-  } from "react-router-dom";
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<Landing />} />
-    )
-)
-
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Landing />} />
+      <Route path="/:locationSlug" element={<LocationDetails />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router}/>
+  <RouterProvider router={router} />
 );
