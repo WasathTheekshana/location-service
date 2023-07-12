0d384d63b5751841f2b4aafc048c1111f0427e93
WasathTheekshana <wasath.vt@gmail.com>
2023-07-12 19:23:29 +0530

    feat(client): device custom card

--- client/src/components/DeviceCard.jsx
+++ client/src/components/DeviceCard.jsx
@@ -1,33 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import ChipButton from "./buttons/ChipButton";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
+import { DeleteButton } from "./buttons/DeleteButton";
 
-const DeviceCard = ({ serialNumber, type, status, image }) => {
+
+const DeviceCard = ({
+  deviceId,
+  locationId,
+  serialNumber,
+  type,
+  status,
+  image,
+  onClick,
+}) => {
+
   return (
-    <Card className="rounded-lg shadow-md hover:transition transition hover:shadow-xl">
-      <CardMedia
-        sx={{ height: 140 }}
-        image={image}
-        title="green iguana"
-      />
-      <CardContent>
-        <h1 className="font-bold text-xl mb-3">{serialNumber}</h1>
-        <h4 className="text-gray-500 text-xs mb-2">{type}</h4>
-        <h4 className="text-gray-500 text-xs">
-          {status ? `Active` : `Inactive`}
-        </h4>
-      </CardContent>
-      <CardActions>
-        <Link to="">
-          <ChipButton />
-        </Link>
-      </CardActions>
-    </Card>
+    <div>
+
+      <Card className="relative rounded-lg shadow-md hover:transition transition hover:shadow-xl">
+        <CardMedia sx={{ height: 140 }} image={image} title="green iguana" />
+        <CardContent>
+          <h1 className="font-bold text-xl mb-3">{serialNumber}</h1>
+          <h4 className="text-gray-500 text-xs mb-2">{type}</h4>
+          <h4 className="text-gray-500 text-xs">
+            {status ? `Active` : `Inactive`}
+          </h4>
+        </CardContent>
+        <CardActions className="absolute z-10 top-1 right-1">
+          <Link to="">
+            <DeleteButton
+              onClick={onClick}
+            />
+          </Link>
+        </CardActions>
+      </Card>
+    </div>
   );
 };
 
