d4c2d06be118e340cbc4c897a4091d6464fb27b0
WasathTheekshana <wasath.vt@gmail.com>
2023-07-13 12:41:29 +0530

    chore(server): body parser payload length maximised

--- server/src/config/express.js
+++ server/src/config/express.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import bodyParser from "body-parser";
 import v1 from "../api/routes/v1/index.js"
 
 const app = express();
@@ -10,6 +11,8 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.disable("x-powered-by");
 app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 //Mount API v1 routes
 app.use("/v1", v1);
