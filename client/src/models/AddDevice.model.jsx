c722e87fe5e55f1702ba961b975e89ef65837ad4
WasathTheekshana <wasath.vt@gmail.com>
2023-07-13 12:40:51 +0530

    style(client): device image upload view better

--- client/src/models/AddDevice.model.jsx
+++ client/src/models/AddDevice.model.jsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react'
-import Model from './Model'
-import NormalButton from '../components/buttons/NormalButton'
-import { useFormik } from 'formik'
-import { converToBase64 } from '../helper/convert'
-import Dummy from '../assets/dummy.png'
-import { addDevice } from '../helper/helper'
-import { toast } from 'react-hot-toast'
+import React, { useState } from "react";
+import Model from "./Model";
+import NormalButton from "../components/buttons/NormalButton";
+import { useFormik } from "formik";
+import { converToBase64 } from "../helper/convert";
+import Dummy from "../assets/dummy.png";
+import { addDevice } from "../helper/helper";
+import { toast } from "react-hot-toast";
 
-const AddDeviceModel = ({isOpen, locationId}) => {
+const AddDeviceModel = ({ isOpen, locationId }) => {
   const [closeModel, setCloseModel] = useState(isOpen);
   const [file, setFile] = useState();
-
 
   const formik = useFormik({
     initialValues: {
@@ -21,22 +20,22 @@ const AddDeviceModel = ({isOpen, locationId}) => {
     validateOnChange: false,
     validateOnBlur: false,
     onSubmit: async (values) => {
-      values = await Object.assign(values, {image: file});
-       console.log(values);
+      values = await Object.assign(values, { image: file });
+      console.log(values);
 
-       let addPostPromise = addDevice(locationId,values)
-       toast.promise(addPostPromise, {
-        loading: 'Adding device...',
-        success: 'Device added successfully',
-        error: 'Error adding device'
-       })
-    }
+      let addPostPromise = addDevice(locationId, values);
+      toast.promise(addPostPromise, {
+        loading: "Adding device...",
+        success: "Device added successfully",
+        error: "Error adding device",
+      });
+    },
   });
 
   const onUpload = async (e) => {
     const base64 = await converToBase64(e.target.files[0]);
     setFile(base64);
-  }
+  };
 
   const refreshPage = () => {
     window.location.reload();
@@ -62,12 +61,22 @@ const AddDeviceModel = ({isOpen, locationId}) => {
         onSubmit={formik.handleSubmit}
         className="w-full flex flex-col gap-5"
       >
-         <div className="profile flex justify-center py-4">
-              <label htmlFor="profile">
-                <img src={file || Dummy} alt="avatar" />
-              </label>
-              <input onChange={onUpload} type="file" id="profile" name="profile" />
-            </div>
+        <div className="profile flex flex-col items-center justify-center py-4">
+          <label htmlFor="profile" className="mb-2">
+            <img
+              src={file || Dummy}
+              alt="avatar"
+              className="w-full h-48 object-cover"
+            />
+          </label>
+          <input
+            onChange={onUpload}
+            type="file"
+            id="profile"
+            name="profile"
+            className="hidden"
+          />
+        </div>
         <input
           {...formik.getFieldProps("serialNumber")}
           type="text"
@@ -77,29 +86,32 @@ const AddDeviceModel = ({isOpen, locationId}) => {
         />
         <select
           {...formik.getFieldProps("type")}
-       name='type' className='w-full border-black border rounded-lg py-2 pl-6'>
+          name="type"
+          className="w-full border-black border rounded-lg py-2 pl-6"
+        >
           <option value="">Select Type</option>
           <option value="pos">POS</option>
           <option value="kisok">KISOK</option>
           <option value="signage">SIGNAGE</option>
         </select>
 
-        <select 
+        <select
           {...formik.getFieldProps("status")}
-        name='status' className='w-full border-black border rounded-lg py-2 pl-6'>
+          name="status"
+          className="w-full border-black border rounded-lg py-2 pl-6"
+        >
           <option value="">Select Status</option>
           <option value={true}>Active</option>
           <option value={false}>Inactive</option>
         </select>
-       
-        
+
         <div className="flex gap-3 justify-start w-full">
           <NormalButton text="Add Location" submit />
           <NormalButton text="Cancel" isBorder onClick={handleClose} />
         </div>
       </form>
     </Model>
-  )
-}
+  );
+};
 
-export default AddDeviceModel
+export default AddDeviceModel;
